007d9d4731786f1aca81101fd9f718253d1a8653
mcabrera <mcabrera>
2024-05-29 00:33:59 -0300

    LPV3-5784/LPV3-5786/CambiosEnPasaporteColombiaYenBeneficiarioNameDecolombia

--- BackgroundSide/SharedModel/ValidationsAttrs/Colombia/IdAttributeColombia.cs
+++ BackgroundSide/SharedModel/ValidationsAttrs/Colombia/IdAttributeColombia.cs
@@ -22,10 +22,15 @@ namespace SharedModel.ValidationsAttrs
                 {//si el tipo de dni el nulo lanzamos exception
                     return new ValidationResult(ErrorMessage = "Parameter::type_of_id :: invalid.#INVALID", new string[] { membername });
                 }
+
                 //segun el tipo de documento es la validacion que se aplica 
                 switch (typeOfIdValue.ToString())
                 {
                     case "1": // '1' - Cédula de ciudadanía 
+                        if (long.Parse(document) <= 9999)
+                        {//si el numero de documento es menor a 9999 se rechaza el payout 
+                            return new ValidationResult(ErrorMessage = "Parameter :: id :: invalid format: only allow numbers and length has 6 or 10 characters.#INVALID", new string[] { membername });
+                        }
                         Regex expresion = new Regex("^[0-9]{6,10}$"); //VALIDA QUE SEA NUMERICO Y QUE TENGA entre 6 y 10 numeros
                         if (!expresion.IsMatch(document))
                         {
@@ -33,6 +38,10 @@ namespace SharedModel.ValidationsAttrs
                         }
                         break;
                     case "2": //'2' - Cédula de extranjería
+                        if (long.Parse(document) <= 9999)
+                        {//si el numero de documento es menor a 9999 se rechaza el payout 
+                            return new ValidationResult(ErrorMessage = "Parameter :: id :: invalid format: only allow numbers and length has 6 or 7 characters.#INVALID", new string[] { membername });
+                        }
                         Regex expresion2 = new Regex("^[0-9]{6,7}$"); //VALIDA QUE SEA NUMERICO Y QUE TENGA 6 numeros
                         if (!expresion2.IsMatch(document))
                         {
@@ -40,6 +49,10 @@ namespace SharedModel.ValidationsAttrs
                         }
                         break;
                     case "3": // '3' - NIT
+                        if (long.Parse(document) <= 9999)
+                        {//si el numero de documento es menor a 9999 se rechaza el payout 
+                            return new ValidationResult(ErrorMessage = "Parameter :: id :: invalid format: only allow numbers and length has between 10 characters.#INVALID", new string[] { membername });
+                        }
                         Regex expresion3 = new Regex("^(?:\\d{3}\\.\\d{3}\\.\\d{3}-\\d|\\d{10}|\\d{6}\\.\\d{4}|\\d{9}\\-\\d{1})$"); // SE VALIDAN 4 TIPOS DE FORMATO
                         //FORMATO 1 -> NNNNNNNNNN
                         //FORMATO 2 -> NNNNNNNNN-N
@@ -51,6 +64,10 @@ namespace SharedModel.ValidationsAttrs
                         }
                         break;
                     case "4": //'4' - Tarjeta de identidad
+                        if (long.Parse(document) <= 9999)
+                        {//si el numero de documento es menor a 9999 se rechaza el payout 
+                            return new ValidationResult(ErrorMessage = "Parameter :: id :: invalid format: only allow numbers and length has between 10 characters.#INVALID", new string[] { membername });
+                        }
                         Regex expresion4 = new Regex("^[0-9]{10}$"); //VALIDA QUE SEA NUMERICO Y QUE TENGA 10 numeros
                         if (!expresion4.IsMatch(document))
                         {
@@ -58,7 +75,7 @@ namespace SharedModel.ValidationsAttrs
                         }
                         break;
                     case "5": //'5' - Pasaporte
-                        Regex expresion5 = new Regex("^[0-9]{5,12}$"); //VALIDA QUE SEA NUMERICO Y QUE TENGA UN MAXIMO ENTRE 5 Y 12 numeros
+                        Regex expresion5 = new Regex("^[A-Za-z0-9-]{5,12}$"); //VALIDA QUE SEA Alfa NUMERICO Y QUE TENGA UN MAXIMO ENTRE 5 Y 12 numeros
                         if (!expresion5.IsMatch(document))
                         {
                             return new ValidationResult(ErrorMessage = "Parameter :: id :: invalid format: only allow numbers and length has between 5 and 12 characters.#INVALID", new string[] { membername });
